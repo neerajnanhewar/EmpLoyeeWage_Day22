e43269facf8000f8bd02f222ea4e93e55e13ed78
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-03 13:07:51 +0530

    Added : Employee wage calculation added

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,20 +1,27 @@
-interface Present{
-	public void PresentCheck();
+
+interface EmployeeWage{
+	public void emp_Wage_Cal();
 } 
-class PresentCheck implements Present{
+class Wage_Cal implements EmployeeWage{
 	public static final int Is_Present = 1 ;
-	double checkPresent = Math.floor(Math.random()*10) % 2 ;
-	public void PresentCheck(){
-		if(checkPresent== Is_Present){
+	public static final int Emp_Rate_PerHr=20;
+	int emp_FullDay_Hr = 0 ;
+	int emp_Wage = 0 ;
+	double empCheck = Math.floor(Math.random()*10) % 2 ;
+	public void emp_Wage_Cal(){
+		if(empCheck == Is_Present){
+			emp_FullDay_Hr=8;
 			System.out.println("Employee is Present");
 		}else{
 			System.out.println("Employee is Absent");
 		}
+		emp_Wage = Emp_Rate_PerHr * emp_FullDay_Hr ;
+		System.out.println("Employee Total wage is " + emp_Wage);
 	}
 }
 class EmployeeWageBuilder {
 	public static void main (String[] args){
-		PresentCheck p_check = new PresentCheck();
-		p_check.PresentCheck();
+		Wage_Cal wagecal= new Wage_Cal();
+		wagecal.emp_Wage_Cal();
 	}
 }
