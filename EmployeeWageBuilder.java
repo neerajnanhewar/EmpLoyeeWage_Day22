2df41dc22123479a9aa30e305d62c2b41cbd5d39
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-03 12:50:47 +0530

    Added : Employee Presentcheck method

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,5 +1,20 @@
+interface Present{
+	public void PresentCheck();
+} 
+class PresentCheck implements Present{
+	public static final int Is_Present = 1 ;
+	double checkPresent = Math.floor(Math.random()*10) % 2 ;
+	public void PresentCheck(){
+		if(checkPresent== Is_Present){
+			System.out.println("Employee is Present");
+		}else{
+			System.out.println("Employee is Absent");
+		}
+	}
+}
 class EmployeeWageBuilder {
 	public static void main (String[] args){
-		System.out.println("Welcome to Employee Wage Computation Program");
+		PresentCheck p_check = new PresentCheck();
+		p_check.PresentCheck();
 	}
 }
