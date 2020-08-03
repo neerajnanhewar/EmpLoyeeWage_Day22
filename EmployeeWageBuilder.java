db367dc78b5574c17c7d17ef890f76af4735123e
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-03 14:24:50 +0530

    Added : Wage calculation for multiple company

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,13 +1,13 @@
 interface EmployeeWage{
-	public void emp_Wage_Cal();
-	public void wage_Compute();
-	public void wage_display();
+	public void emp_Wage_Cal(String company, int Emp_Rate_PerHr);
+	public void wage_Compute(int Emp_Rate_PerHr);
+	public void wage_display(String company);
 }
 
 class Wage_Cal implements EmployeeWage{
 	public static final int Is_Full_Time = 1 ;
 	public static final int Is_Part_Time = 2;
-	public static final int Emp_Rate_PerHr=20;
+	//public static final int Emp_Rate_PerHr=20;
 	public static final int Total_Work_Day = 20;
 	static final int Total_Working_Hr_Month = 100;
 	int emp_FullDay_Hr = 0 ;
@@ -15,15 +15,16 @@ class Wage_Cal implements EmployeeWage{
 	int total_EmpWage=0;		
 	int total_working_day=0;
 	int total_empHrs = 0;
-	public void wage_Compute(){
+	public void wage_Compute(int Emp_Rate_PerHr){
 			emp_Wage = Emp_Rate_PerHr * emp_FullDay_Hr ;		
-			System.out.println("Employee's [day-" + total_working_day + "] wage is " + emp_Wage);
+		//	System.out.println("Employee's [day-" + total_working_day + "] wage is " + emp_Wage);
 	}
-	public void wage_display(){
+	public void wage_display(String company){
+		System.out.println("Wages for " + company);
 		System.out.println("Totla Working hours " + total_empHrs );
 		System.out.println("Employee Total wage For a Month : " + total_EmpWage);
 	}
-	public void emp_Wage_Cal(){
+	public void emp_Wage_Cal(String company, int Emp_Rate_PerHr){
 		while(total_working_day < Total_Work_Day && total_empHrs < Total_Working_Hr_Month ){
 			total_working_day++;
 			int empCheck = (int) Math.floor(Math.random()*10) % 3 ;		
@@ -40,14 +41,16 @@ class Wage_Cal implements EmployeeWage{
 			}
 			total_EmpWage += emp_Wage ;
 			total_empHrs += emp_FullDay_Hr;
-			wage_Compute();	
+			wage_Compute(Emp_Rate_PerHr);	
 		}	
-			wage_display();
+			wage_display(company);
 	}
 }
 class EmployeeWageBuilder {
 	public static void main (String[] args){
-		Wage_Cal wagecal= new Wage_Cal();
-		wagecal.emp_Wage_Cal();
+		Wage_Cal d_wagecal= new Wage_Cal();
+		Wage_Cal k_wagecal= new Wage_Cal();
+		d_wagecal.emp_Wage_Cal("Dmart",20);
+		k_wagecal.emp_Wage_Cal("Kmart",20);
 	}
 }
